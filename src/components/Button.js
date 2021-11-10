71eb76cced201d28eda6e34f3ff0ca7e182325d2
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2021-11-09 23:20:17 -0500

    first testing complete for onClick with confirm and disable with danger

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,12 +5,18 @@ import "components/Button.scss";
 export default function Button(props) {
   let buttonClass = "button";
 
+  //   Confirm uses the confirm prop to apply the .button--confirm modifier class
   if (props.confirm) {
-    buttonClass += "button--confirm";
+    buttonClass += " button--confirm";
   }
+  //   Danger uses the danger prop to apply the .button--danger modifier class
   if (props.danger) {
-    buttonClass += "button--danger";
+    buttonClass += " button--danger";
   }
 
-  return <button className={buttonClass}>{props.children}</button>;
+  return (
+    <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>
+      {props.children}
+    </button>
+  );
 }

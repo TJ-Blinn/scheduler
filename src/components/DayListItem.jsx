1769002e9717ee5c2a53a01f221d6926f3343410
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2021-11-11 14:22:35 -0500

    added formatSpots in the DayListItem component that will format the props.spots to show 0 spots, 1 spot and many spots remaining

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -6,17 +6,40 @@ import classNames from "classnames";
 // The <h2> should display the day name
 // The <h3> should display the spots remaining for a day
 
-export default function DayLisItem(props) {
+// DayList Item is requiring arguments passed into it called "props'"
+// we then destructuring the spots parameter which was passed the singular values of spots
+// taking out spots arguments from the list of arguments "props" (in the DayListItem function)
+
+
+export default function DayListItem(props) {
+  // console.log(props);
+  const {spots} = props
+  // console.log(spots);
+  // console.log(name);
+  // console.log(selected);
+
+  function formatSpots(spots) {
+    if (spots === 1) {
+      return `${spots} spot remaining`
+    } 
+    if (spots > 1) {
+      return `${spots} spots remaining`
+    }
+    return `no spots remaining`
+  
+ }
+
+ const availableSpots = formatSpots(spots)
+
   const dayClass = classNames("day-list__item", {
     " day-list__item--full": props.spots === 0,
     " day-list__item--selected": props.selected,
-  });
-  
+  });  
   
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{availableSpots}</h3>
     </li>
   );
 }

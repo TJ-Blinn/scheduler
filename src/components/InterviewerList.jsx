a42ec17e5eee52d6853ac306d778a3204c6909bd
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2021-11-12 14:48:12 -0500

    refactored InterviewerList to render InterviewerListItem component, added key

---
+++ src/components/InterviewerList.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import classNames from "classnames";
+import "./InterviewerList.scss"
+import InterviewerListItem from "./InterviewerListItem";
+
+export default function InterviewerList(props) {
+  console.log("PROPS -----------------", props);
+
+  // const {interviewers, setInterviewer, interviewer} = props;
+
+  const interviewerComponent = props.interviewers.map(interviewer => {
+    // console.log(interviewer);
+    return <InterviewerListItem
+    key={interviewer.id}
+    name={interviewer.name}
+    avatar={interviewer.avatar}
+    selected={interviewer.id === props.interviewer}
+    setInterviewer={props.setInterviewer}
+    />
+  })
+  
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewer__list">{interviewerComponent} </ul>
+    </section>
+  )
+}

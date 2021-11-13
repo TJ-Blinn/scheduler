49112c302e4b42b0cd3f63cfcaae3d198983cbdb
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2021-11-13 14:04:32 -0500

    added Header component

---
+++ src/components/Appointment/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// * styles is referenced in stories/index.js file, we don't need to import it again
+
+export default function Header(props) {
+  return (
+    <header className="appointment__time">
+  <h4 className="text--semi-bold">12pm</h4>
+  <hr className="appointment__separator" />
+</header>
+
+  )
+}

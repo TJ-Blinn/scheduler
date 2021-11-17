4602d1dd9f64a9da7b58642b7a59e33f4670981c
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2021-11-16 19:13:16 -0500

    added Iterating Over Appointments and mock data for appointment rendering

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 // import React from "react";
 import DayList from "./DayList";
 import React, { useState } from "react";
+import Appointment from "./Appointment";
 
 import "components/Application.scss";
 
@@ -22,9 +23,55 @@ const days = [
   },
 ];
 
+// Mock Data
+const appointments = {
+  1: {
+    id: 1,
+    time: "12pm",
+  },
+  2: {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  3: {
+    id: 3,
+    time: "2pm",
+  },
+  4: {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      },
+    },
+  },
+  5: {
+    id: 5,
+    time: "4pm",
+  },
+};
+
+// end of Mock Data
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
   console.log("day console log: ", day);
+
+  const appointmentList = Object.values(appointments).map((appointment) => {
+    return <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={appointment.interview} />;
+  });
 
   return (
     <main className="layout">
@@ -36,7 +83,7 @@ export default function Application(props) {
         </nav>
         <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs" />
       </section>
-      <section className="schedule">{/* Replace this with the schedule elements durint the "The Scheduler" activity. */}</section>
+      <section className="schedule">{appointmentList}</section>
     </main>
   );
 }

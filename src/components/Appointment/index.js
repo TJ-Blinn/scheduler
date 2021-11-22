c895f0ce6bd65e364ae7284d7f899e1ef8902612
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2021-11-21 22:12:50 -0500

    added custom hooks for Appointment for Empty, Show, and Create components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,18 +1,30 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
   // Conditional Expressions using Fragments o render <Show> or <Empty> based on props.interview.
+
+  const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
+
   return (
     <>
       <article className="appointment">
         {/* {appointmentString()} */}
         <Header time={props.time} />
-        {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+        {/* {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />} */}
+        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+        {mode === SHOW && <Show student={props.interview.student} interviewer={props.interview.interviewer} />}
+        {mode === CREATE && <Form interviewers={[]} onCancel={() => back(EMPTY)} />}
       </article>
     </>
   );

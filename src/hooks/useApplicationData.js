b0e1969530715d34b4bcba150151c81815f57e00
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2021-12-03 14:46:03 -0500

    added proxy for /api in package.json and updates urls in project

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -77,7 +77,7 @@ export default function useApplicationData(initial) {
       }
     });
 
-    return axios.delete(`http://localhost:8001/api/appointments/${id}`).then((response) => {
+    return axios.delete(`/api/appointments/${id}`).then((response) => {
       setState({
         ...state,
         appointments: newAppointments,
@@ -90,12 +90,8 @@ export default function useApplicationData(initial) {
 
   // API calls
   useEffect(() => {
-    Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers"),
-    ]).then((all) => {
-      // console.log(all); //
+    Promise.all([axios.get("/api/days"), axios.get("/api/appointments"), axios.get("/api/interviewers")]).then((all) => {
+      console.log(all); //
       setState((prev) => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
   }, []);

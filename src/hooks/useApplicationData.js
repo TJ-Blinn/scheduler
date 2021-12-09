8c23f9f53edacec1fa84502a6302465ff8b3b82c
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2021-12-08 22:01:40 -0500

    update newDays var adding a ternary to confirm to only reduce spots by 1 if an interview is not present

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -34,15 +34,15 @@ export default function useApplicationData(initial) {
     const newDays = [];
     state.days.forEach((dayObj) => {
       if (dayObj.name === state.day) {
-        console.log(dayObj.name, ":", dayObj.spots);
-        console.log("selected day = : ", state.day);
-        newDays.push({ ...dayObj, spots: dayObj.spots - 1 });
+        // console.log(dayObj.name, ":", dayObj.spots);
+        // console.log("selected day = : ", state.day);
+        newDays.push({ ...dayObj, spots: state.appointments[id].interview ? dayObj.spots : dayObj.spots - 1 });
       } else {
         newDays.push({ ...dayObj });
       }
     });
 
-    return axios.put(`http://localhost:8001/api/appointments/${id}`, { interview }).then((response) => {
+    return axios.put(`api/appointments/${id}`, { interview }).then((response) => {
       setState({
         ...state,
         appointments: newAppointments,
@@ -69,8 +69,8 @@ export default function useApplicationData(initial) {
     const newDays = [];
     state.days.forEach((dayObj) => {
       if (dayObj.name === state.day) {
-        console.log(dayObj.name, ":", dayObj.spots);
-        console.log("selected day = : ", state.day);
+        // console.log(dayObj.name, ":", dayObj.spots);
+        // console.log("selected day = : ", state.day);
         newDays.push({ ...dayObj, spots: dayObj.spots + 1 });
       } else {
         newDays.push({ ...dayObj });
@@ -91,7 +91,7 @@ export default function useApplicationData(initial) {
   // API calls
   useEffect(() => {
     Promise.all([axios.get("/api/days"), axios.get("/api/appointments"), axios.get("/api/interviewers")]).then((all) => {
-      console.log(all); //
+      // console.log(all); //
       setState((prev) => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
   }, []);

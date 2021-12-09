aeba2ce9fbc788fb8a77ddb655fd7ab3eccbd3b4
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2021-12-09 11:54:09 -0500

    add 1st Cypress test to navigate day-list_item and click

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -6,9 +6,7 @@ describe("Navigation", () => {
   it("should Navigate to Tuesday", () => {
     cy.visit("/");
     // get the list item that contains the text "Tuesday" and clicks on it.
-    cy.get("li").contains("Tuesday").click();
-
     // verify that the day list item has a specific value for the background-color CSS property
-    cy.contains("li", "Tuesday").should("have.css", "background-color", "rgb(242, 242, 242)");
+    cy.contains("[data-testid=day]", "Tuesday").click().should("have.class", "day-list__item--selected");
   });
 });

e9ebf8e65b50bb0733dad710b14e5f7ce3508719
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2021-12-10 16:43:01 -0500

    corrected tests for form to include missing interviewer

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { render, cleanup, fireEvent } from "@testing-library/react";
+import { render, cleanup, fireEvent, prettyDOM } from "@testing-library/react";
 import Form from "components/Appointment/Form";
 // import Appointment from "components/Appointment";
 
@@ -9,7 +9,7 @@ describe("Form", () => {
   const interviewers = [
     {
       id: 1,
-      student: "Sylvia Palmer",
+      name: "Sylvia Palmer",
       avatar: "https://i.imgur.com/LpaY82x.png",
     },
   ];
@@ -35,6 +35,7 @@ describe("Form", () => {
 
     /* 3. Click the save button */
     fireEvent.click(getByText("Save"));
+    // console.log("Get by text ---------", prettyDOM(getByText));
     expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
 
     /* onSave is not called */
@@ -44,7 +45,7 @@ describe("Form", () => {
   // Refactored Test 3
   it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
-    const { getByText, getByPlaceholderText, queryByText } = render(<Form interviewers={interviewers} onSave={onSave} />);
+    const { getByText, getByPlaceholderText, queryByText, getByAltText } = render(<Form interviewers={interviewers} onSave={onSave} />);
 
     fireEvent.click(getByText("Save"));
 
@@ -55,12 +56,12 @@ describe("Form", () => {
       target: { value: "Lydia Miller-Jones" },
     });
 
+    fireEvent.click(getByAltText("Sylvia Palmer"));
     fireEvent.click(getByText("Save"));
-
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
 
     expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
   });
 
   // Refactored Test 4
